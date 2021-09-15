0ee0931c905633c8295fe16b2da30011e08f5287
Hackaman <43167327+sanjeevminati@users.noreply.github.com>
2021-09-15 18:50:43 +0530

    Update admin.py
    for the image preview in admin page

--- Caption_Project/index/admin.py
+++ Caption_Project/index/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Image,Caption   
-# Register your models here.
-admin.site.register(Image)
-admin.site.register(Caption)
+from .models import *
+from django.utils.html import format_html
+
+class GalleryAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.imageURL.url))
+
+    image_tag.short_description = 'Image Preview'
+
+    readonly_fields = ['image_tag']
+
+
+class ImageAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="auto" height="200px" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image Preview'
+
+    readonly_fields = ['image_tag']
+
+admin.site.register(Image,ImageAdmin)
+admin.site.register(Caption,GalleryAdmin)
